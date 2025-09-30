9cb22bbfa0b1b9d2d4cfe226c1976f908baf27eb
soo-10 <uoosoo0723@gmail.com>
2025-09-30 09:30:08 +0900

    updeta
    나머지가 1이 되는 수 찾기
    문제풀이

--- Solution.cpp
+++ Solution.cpp
@@ -1,4 +1,6 @@
+
 #include <iostream>
+/*
 #include <string>
 #include <vector>
 
@@ -18,7 +20,7 @@ int solution(int n)
     }
     return answer;
 }
-int main() 
+int main()
 {
     int n;
     cout << " [숫자 입력]:";
@@ -26,5 +28,38 @@ int main()
 
     cout << " [약수의 합]:" << solution(n);
 
+    return 0;
+}
+*/
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n)
+{
+    int answer = 0;
+    for (int x = 2; x < n; ++x)
+    {
+
+        if (n % x == 1)
+        {
+            answer = x;
+            break;
+        }
+
+
+    }
+
+    return answer;
+}
+int main() {
+    int n;
+
+    cout << " [숫자 입력]:";
+    cin >> n;
+
+    cout << " [나머지가 1이 되는 수]:" << solution(n);
     return 0;
 }
